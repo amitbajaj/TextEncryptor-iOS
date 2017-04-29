6fd34f38843b7e6a85fdc04bb5497a00c2bc3258
Amit Bajaj <amitbajaj100@yahoo.com>
2017-04-29 10:16:23 +0300

    Initial Commit

---
+++ TextEncryptor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextEncryptor
+//
+//  Created by Amit Bajaj on 4/29/17.
+//  Copyright © 2017 online.buzzzz.security. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
