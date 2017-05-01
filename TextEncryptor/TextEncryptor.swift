ed9f3e1136976abe5eb3f29fff3065d951820133
Amit Bajaj <amitbajaj100@yahoo.com>
2017-05-01 23:18:47 +0300

    Testing the layouts

---
+++ TextEncryptor/TextEncryptor.swift
@@ -0,0 +1,17 @@
+//
+//  TextEncryptor.swift
+//  TextEncryptor
+//
+//  Created by Amit Bajaj on 5/1/17.
+//  Copyright © 2017 online.buzzzz.security. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextEncryptor: UITableViewController{
+    @IBAction func setText(){
+        
+    }
+
+}
